4ca96a3e6e64e007816b936af22771212f5e2c31
Omotayo Philip Oyegoke <oyegokeomotayo@gmail.com>
2026-07-28 01:16:27 +0100

    Configure frontend environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ export default function App() {
     setTyping(true);
 
 try {
-  const res = await fetch("http://localhost:5000/chat", {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/chat`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
